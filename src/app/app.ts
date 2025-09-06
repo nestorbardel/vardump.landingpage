98177a77c8d747873dbafe0f70ef08fa6a359b18
nestorbardel <nestorbardel@gmail.com>
2025-09-06 19:42:34 -0300

    feat: add Hero component with styles and animations
    - Implemented Hero component for landing page.
    - Added HTML, SCSS, and TS files with layout, animations, and styling.
    - Integrated Hero component into the app.
    - Updated dependencies to include `@angular/animations`.

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { NavbarComponent } from "./components/navbar/navbar";
+import {Hero} from './components/hero/hero/hero';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, NavbarComponent],
+  imports: [RouterOutlet, NavbarComponent, Hero],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
