34d5cd3e04ad3741c1fe68b514bce629a6905158
nestorbardel <nestorbardel@gmail.com>
2025-09-07 14:16:52 -0300

    feat: update branding and improve Hero section
    - Updated page title and added favicon to index.html.
    - Modified Hero section layout: disabled "Ver trabajos" button and updated "Enviar mensaje" button to primary style.
    - Removed unused GitHub icon import from Footer component.
    - Added `vardump.ico` asset file.

--- src/app/components/footer/footer.ts
+++ src/app/components/footer/footer.ts
@@ -1,12 +1,10 @@
 import { Component } from '@angular/core';
 import {LucideAngularModule} from 'lucide-angular';
-import { SiGithubIcon } from '@semantic-icons/simple-icons';
 
 @Component({
   selector: 'app-footer',
   imports: [
     LucideAngularModule,
-    SiGithubIcon,
   ],
   templateUrl: './footer.html',
   styleUrl: './footer.scss'
