49604363fb52720de67dd10c4957ae84cc8337d2
nestorbardel <nestorbardel@gmail.com>
2025-09-08 11:46:26 -0300

    feat: enhance ContactForm with feedback messages and HTTP integration

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -2,6 +2,7 @@ import { ApplicationConfig, provideBrowserGlobalErrorListeners, provideZonelessC
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
+import { provideHttpClient } from '@angular/common/http';
 
 
 export const appConfig: ApplicationConfig = {
@@ -9,5 +10,6 @@ export const appConfig: ApplicationConfig = {
     provideBrowserGlobalErrorListeners(),
     provideZonelessChangeDetection(),
     provideRouter(routes),
+    provideHttpClient(),
   ]
 };
--- src/app/components/contact-form/contact-form.ts
+++ src/app/components/contact-form/contact-form.ts
@@ -1,17 +1,25 @@
-import {Component, signal, WritableSignal} from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { HttpClient } from '@angular/common/http';
+import {Component, inject, signal, WritableSignal} from '@angular/core';
 import {FormsModule} from '@angular/forms';
-
+import { firstValueFrom } from 'rxjs';
+interface Feedback {
+  type: 'success' | 'error';
+  text: string;
+}
 @Component({
   selector: 'app-contact-form',
-  imports: [FormsModule],
+  imports: [FormsModule, CommonModule],
   templateUrl: './contact-form.html',
   styleUrl: './contact-form.scss'
 })
 export class ContactForm {
+  http = inject(HttpClient);
   name: WritableSignal<string> = signal('');
   email: WritableSignal<string> = signal('');
   message: WritableSignal<string> = signal('');
   submitted: WritableSignal<boolean> = signal(false);
+  feedback: WritableSignal<Feedback | null> = signal(null);
 
   onInputChangeName(event: Event){
     const target = event.target as HTMLInputElement;
@@ -29,14 +37,38 @@ export class ContactForm {
     this.message.set(value);
   }
 
-  onSubmit(){
-    if(!this.name() || !this.email() || !this.message() ) return;
-    this.submitted.set(true);
+  async onSubmit(){
+if (!this.name() || !this.email() || !this.message()) {
+      this.feedback.update(() => ({
+        type: 'error',
+        text: 'Por favor, completa todos los campos.'
+      }));
+      return;
+    }
+      this.submitted.set(true);
+    try {
+      await firstValueFrom(this.http.post('http://localhost:3000/send-email', {
+        to: "nestorbardel@gmail.com",
+        subject: "New message from contact form",
+        body:`
+        Name: ${this.name()}
+        Email: ${this.email()}
+        Message: ${this.message()}
+        `,
+    }));
+    this.feedback.update(() => ({
+        type: 'success',
+        text: `Gracias ${this.name()}, tu mensaje fue enviado.`
+      }));
+      this.name.set('');
+      this.email.set('');
+      this.message.set('');
+      this.submitted.set(false);
+    } catch (error) {
+      console.log(error);
 
-    console.log('Formulario enviado: ', {
-      name: this.name(),
-      email: this.email(),
-      message: this.message()
-    })
+    }
+
+
   }
 }
